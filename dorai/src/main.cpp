37b6be33031926823505faa39389e155dd5abbb9
donguk lim <donguk.lim@nota.ai>
2021-03-16 23:07:04 +0900

    초기 커밋.

---
+++ dorai/src/main.cpp
@@ -0,0 +1,59 @@
+//#include "connected_layer.h"
+//#include "convolutional_layer.h"
+//#include "maxpool_layer.h"
+//#include "network.h"
+//#include "image.h"
+
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "opencv2/opencv.hpp"
+
+using namespace cv;
+
+void test_convolve()
+{
+}
+
+void test_color()
+{
+}
+
+void test_convolutional_layer()
+{
+}
+
+void test_load()
+{
+}
+void test_upsample()
+{
+}
+
+void test_rotate()
+{
+}
+
+void test_network()
+{
+}
+void test_backpropagate()
+{
+}
+
+int main()
+{
+    //test_backpropagate();
+    //test_convolve();
+    //test_upsample();
+    //test_rotate();
+    //test_load();
+    test_network();
+    //test_convolutional_layer();
+    //test_color();
+    WaitKey(0);
+    cv::WaitKey(0);
+    
+    return 0;
+}
